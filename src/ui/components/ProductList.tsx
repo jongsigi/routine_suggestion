58a5047915182adcb212a93e9df4e896e030728e
jongsigi <jongsigi500@gmail.com>
2024-09-08 12:33:23 +0900

    240908 Apply ProductList & Edit json data

--- src/ui/components/ProductList.tsx
+++ src/ui/components/ProductList.tsx
@@ -1,21 +1,22 @@
-// import { ProductElement } from "./ProductElement";
-// import { type ProductListItemFragment } from "@/gql/graphql";
+import { ProductElement } from "./ProductElement";
 
-// export const ProductList = ({ products }: { products: readonly ProductListItemFragment[] }) => {
-// 	return (
-// 		<ul
-// 			role="list"
-// 			data-testid="ProductList"
-// 			className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3"
-// 		>
-// 			{products.map((product, index) => (
-// 				<ProductElement
-// 					key={product.id}
-// 					product={product}
-// 					priority={index < 2}
-// 					loading={index < 3 ? "eager" : "lazy"}
-// 				/>
-// 			))}
-// 		</ul>
-// 	);
-// };
+export function ProductList({ types }) {
+  const typelist = Array.isArray(types) ? types : [];
+
+  return (
+    <ul
+      role="list"
+      data-testid="ProductList"
+      className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3"
+    >
+      {typelist.map((typeData, index) => (
+        <ProductElement
+          key={typeData.id}
+          typeData={typeData}
+          priority={index < 2}
+          loading={index < 3 ? "eager" : "lazy"}
+        />
+      ))}
+    </ul>
+  );
+}
--- src/ui/components/nav/components/NavLinks.tsx
+++ src/ui/components/nav/components/NavLinks.tsx
@@ -1,27 +1,25 @@
 import Link from "next/link";
 import { NavLink } from "./NavLink";
-import navData from '@/../public/navData.json'
+import navData from "@/../public/navData.json";
 
-export function NavLinks ({ channel }: { channel: string }) {
-	// navLinks는 GraphQL의 변수
-	const navLinks = navData
+export function NavLinks({ channel }: { channel: string }) {
+  // navLinks는 GraphQL의 변수
+  const navLinks = navData;
 
-	console.log(navLinks)
-
-	return (
-		<>
-			<NavLink href="/">All</NavLink>
-			{navLinks.menu?.items?.map((item) => {
-				if (item.category) {
-					return (
-						<NavLink key={item.id} href={`/${item.category.slug}`}>
-							{item.category.name}
-						</NavLink>
-					);
-				}
-				else{ return null ;}
-				
-			})}
-		</>
-	);
-};
+  return (
+    <>
+      <NavLink href="/">All</NavLink>
+      {navLinks.menu?.items?.map((item) => {
+        if (item.category) {
+          return (
+            <NavLink key={item.id} href={`/${item.category.slug}`}>
+              {item.category.name}
+            </NavLink>
+          );
+        } else {
+          return null;
+        }
+      })}
+    </>
+  );
+}
