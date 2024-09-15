269abf83279c10055198d7664492b9021fd6ec15
jongsigi <jongsigi500@gmail.com>
2024-09-15 22:19:53 +0900

    0915 SkinTypeDescription Guide

--- src/app/[channel]/(main)/skintypes/[...slug]/page.tsx
+++ src/app/[channel]/(main)/skintypes/[...slug]/page.tsx
@@ -1,13 +1,12 @@
-import {SkinTypeHeader} from "@/ui/components/SkinTypeHeader"
+import { SkinTypeHeader } from "@/ui/components/SkinTypeHeader";
 import SkinTypesCollection from "@public/SkinTypesCollection.json";
 
 export default async function Docs({ params }: { params: { slug: string[] } }) {
-  const matchingNode = await SkinTypesCollection.collection?.types.edges.find(edge => {
-    // Assuming node has a `slug` property to match against
-    return edge.node.slug === params.slug[0];
-  })?.node; // Access the `node` if a match is found
-
-  return(
-    <SkinTypeHeader matchingType={matchingNode}/>
-  )
+  const matchingNode = await SkinTypesCollection.collection?.types.edges.find(
+    (edge) => {
+      // Assuming node has a `slug` property to match against
+      return edge.node.slug === params.slug[0];
+    }
+  )?.node; // Access the `node` if a match is found
+  return <SkinTypeHeader matchingType={matchingNode} />;
 }
--- src/ui/components/SkinTypeHeader.tsx
+++ src/ui/components/SkinTypeHeader.tsx
@@ -1,6 +1,7 @@
 import { ExternalLink } from "@/ui/components/external-link";
-import { SkinTypesProps } from "@/types/SkinTypeListDocument";
+// import { SkinTypesProps } from "@/types/SkinTypeListDocument";
 import { ProductImageWrapper } from "@/ui/atoms/ProductImageWrapper";
+import typesDescriptions from "@public/SkinTypeDescriptions.json";
 
 const navItem = [
   "/img/DRPW.webp",
@@ -10,82 +11,86 @@ const navItem = [
 ];
 
 export function SkinTypeHeader({ matchingType }: { matchingType: any }) {
+  const SkinTypeDescription = typesDescriptions?.find((typeDescription) => {
+    return typeDescription.type === matchingType.slug;
+  });
+
   return (
     <div className="mx-auto max-w-2xl px-4 mt-4">
       <div className="flex flex-col gap-2 rounded-2xl bg-zinc-50 sm:p-8 p-4 text-sm sm:text-base">
         <h1 className="text-2xl sm:text-2xl tracking-tight font-bold max-w-fit inline-block capitalize">
-          {matchingType.id} : {matchingType.slug}
+          {SkinTypeDescription?.id} : {SkinTypeDescription?.type}
         </h1>
-        <p>여기에 피부타입에 대한 간단한 설명</p>
+        <p>
+          한국의 <span className="text-blue-500">{SkinTypeDescription?.percentage}</span>에 해당하는 피부 타입으로,
+          {SkinTypeDescription?.summary}
+        </p>
         <p className="leading-normal text-zinc-900">
           더 자세한 사항은{" "}
           <ExternalLink
             href={
               "https://skintypesolutions.com/blogs/skincare/baumann-skin-" +
-              matchingType.id +
+              SkinTypeDescription?.id +
               "-" +
-              matchingType.slug
+              SkinTypeDescription?.type
             }
           >
             <span className="font-semibold">
               {" "}
-              {matchingType.slug} 피부타입 설명(English)
+              {SkinTypeDescription?.type} 피부타입 설명(English)
             </span>
           </ExternalLink>
           를 통해 확인하실 수 있습니다.
         </p>
         <h1 className="text-2xl sm:text-2xl tracking-tight font-bold max-w-fit inline-block capitalize mt-4">
-          {matchingType.slug} 피부타입의 장단점
+          {SkinTypeDescription?.type} 피부타입의 장단점
         </h1>
-        <p>장점 :</p>
-        <p>단점 :</p>
+        <p>{SkinTypeDescription?.pros}</p>
+        <p>{SkinTypeDescription?.cons}</p>
         <h1 className="text-2xl sm:text-2xl tracking-tight font-bold max-w-fit inline-block capitalize mt-4">
-          {matchingType.slug}이 쓰면 좋은 성분들
+          {SkinTypeDescription?.type}이 쓰면 좋은 성분들
         </h1>
-        <p>여기에 피부타입 장단점</p>
+        <p>{SkinTypeDescription?.recommendedIngredients}</p>
       </div>
       <div className="flex flex-col gap-2 rounded-2xl bg-zinc-50 sm:p-8 p-4 text-sm sm:text-base mt-4">
         <div className="morning-routine">
           <h1 className="text-2xl sm:text-2xl tracking-tight font-bold max-w-fit inline-block capitalize mt-4">
             {matchingType.slug} 추천 모닝루틴
           </h1>
-          <p>
-            <div className="whitespace-nowrap overflow-x-auto flex">
-              {navItem.map((item, index) => {
-                return (
-                  <ProductImageWrapper
-                    key={index}
-                    src={item}
-                    alt={""}
-                    width={8}
-                    height={8}
-                    sizes={"256px"}
-                  />
-                );
-              })}
-            </div>
-          </p>
+
+          <div className="whitespace-nowrap overflow-x-auto flex">
+            {navItem.map((item, index) => {
+              return (
+                <ProductImageWrapper
+                  key={index}
+                  src={item}
+                  alt={""}
+                  width={8}
+                  height={8}
+                  sizes={"256px"}
+                />
+              );
+            })}
+          </div>
         </div>
         <div className="night-routine">
           <h1 className="text-2xl sm:text-2xl tracking-tight font-bold max-w-fit inline-block capitalize mt-4">
             {matchingType.slug} 추천 나이트루틴
           </h1>
-          <p>
-            <div className="whitespace-nowrap overflow-x-auto flex">
-              {navItem.map((item, index) => {
-                return (
-                  <ProductImageWrapper
-                    key={index}
-                    src={item}
-                    alt={""}
-                    width={8}
-                    height={8}
-                    sizes={"256px"}
-                  />
-                );
-              })}
-            </div>
-          </p>
+          <div className="whitespace-nowrap overflow-x-auto flex">
+            {navItem.map((item, index) => {
+              return (
+                <ProductImageWrapper
+                  key={index}
+                  src={item}
+                  alt={""}
+                  width={8}
+                  height={8}
+                  sizes={"256px"}
+                />
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
