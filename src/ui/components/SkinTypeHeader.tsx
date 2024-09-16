f97942bfd6e36fb3a4f08bf94628ab10b8abc0d9
jongsigi <jongsigi500@gmail.com>
2024-09-16 16:52:33 +0900

    240916 description page fix

--- src/ui/components/SkinTypeHeader.tsx
+++ src/ui/components/SkinTypeHeader.tsx
@@ -1,7 +1,7 @@
 import { ExternalLink } from "@/ui/components/external-link";
 // import { SkinTypesProps } from "@/types/SkinTypeListDocument";
 import { ProductImageWrapper } from "@/ui/atoms/ProductImageWrapper";
-import typesDescriptions from "@public/SkinTypeDescriptions.json";
+import typesDescriptions from "@public/SkinTypeDescriptions_t.json";
 
 const navItem = [
   "/img/DRPW.webp",
@@ -12,45 +12,54 @@ const navItem = [
 
 export function SkinTypeHeader({ matchingType }: { matchingType: any }) {
   const SkinTypeDescription = typesDescriptions?.find((typeDescription) => {
-    return typeDescription.type === matchingType.slug;
+    return typeDescription.name === matchingType.slug;
   });
+
+  const skintype = SkinTypeDescription?.name
+  const percentage = SkinTypeDescription?.per
+  const skintype_id = SkinTypeDescription?.id
+  const kor_translate = SkinTypeDescription?.kor_translate
+  const skin_barrier = SkinTypeDescription?.barrier
 
   return (
     <div className="mx-auto max-w-2xl px-4 mt-4">
       <div className="flex flex-col gap-2 rounded-2xl bg-zinc-50 sm:p-8 p-4 text-sm sm:text-base">
         <h1 className="text-2xl sm:text-2xl tracking-tight font-bold max-w-fit inline-block capitalize">
-          {SkinTypeDescription?.id} : {SkinTypeDescription?.type}
+          {skintype_id} : {skintype}
         </h1>
         <p>
-          한국의 <span className="text-blue-500">{SkinTypeDescription?.percentage}</span>에 해당하는 피부 타입으로,
-          {SkinTypeDescription?.summary}
+          한국의 <span className="text-xl font-semibold text-blue-500">{percentage}%</span>에 해당하는 피부타입이다. {' '}
+          {skintype} 피부타입은 {' '}
+          <span className="text-lg font-semibold text-blue-500">{kor_translate}</span>의 특성을 띄는 피부타입으로, 
+          주요한 피부 건강 취약점으로는 <span className="text-lg font-semibold text-red-500">{skin_barrier}</span>이 있다.
         </p>
         <p className="leading-normal text-zinc-900">
           더 자세한 사항은{" "}
           <ExternalLink
             href={
               "https://skintypesolutions.com/blogs/skincare/baumann-skin-" +
-              SkinTypeDescription?.id +
+              skintype_id +
               "-" +
-              SkinTypeDescription?.type
+              skintype
             }
           >
             <span className="font-semibold">
               {" "}
-              {SkinTypeDescription?.type} 피부타입 설명(English)
+              {skintype} 피부타입 설명(English)
             </span>
           </ExternalLink>
           를 통해 확인하실 수 있습니다.
         </p>
-        <h1 className="text-2xl sm:text-2xl tracking-tight font-bold max-w-fit inline-block capitalize mt-4">
-          {SkinTypeDescription?.type} 피부타입의 장단점
+        {/* <h1 className="text-2xl sm:text-2xl tracking-tight font-bold max-w-fit inline-block capitalize mt-4">
+          {skintype} 피부타입의 장단점
         </h1>
         <p>{SkinTypeDescription?.pros}</p>
         <p>{SkinTypeDescription?.cons}</p>
+        */}
         <h1 className="text-2xl sm:text-2xl tracking-tight font-bold max-w-fit inline-block capitalize mt-4">
-          {SkinTypeDescription?.type}이 쓰면 좋은 성분들
+          Enhancing Ingredients : {skintype}
         </h1>
-        <p>{SkinTypeDescription?.recommendedIngredients}</p>
+        <p>{SkinTypeDescription?.recommendedIngredients}</p> 
       </div>
       <div className="flex flex-col gap-2 rounded-2xl bg-zinc-50 sm:p-8 p-4 text-sm sm:text-base mt-4">
         <div className="morning-routine">
