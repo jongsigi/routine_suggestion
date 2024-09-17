3349c44a6ae839d3eccf42596cc0650c5bb3a8ed
jongsigi <jongsigi500@gmail.com>
2024-09-17 13:56:04 +0900

    240917 Edit Description Page (Delete Ingredient Info.)

--- src/ui/components/SkinTypeHeader.tsx
+++ src/ui/components/SkinTypeHeader.tsx
@@ -1,7 +1,7 @@
 import { ExternalLink } from "@/ui/components/external-link";
 // import { SkinTypesProps } from "@/types/SkinTypeListDocument";
 import { ProductImageWrapper } from "@/ui/atoms/ProductImageWrapper";
-import typesDescriptions from "@public/SkinTypeDescriptions_t.json";
+import typesDescriptions from "@public/SkinTypeDescriptions.json";
 
 const navItem = [
   "/img/DRPW.webp",
@@ -15,23 +15,32 @@ export function SkinTypeHeader({ matchingType }: { matchingType: any }) {
     return typeDescription.name === matchingType.slug;
   });
 
-  const skintype = SkinTypeDescription?.name
-  const percentage = SkinTypeDescription?.per
-  const skintype_id = SkinTypeDescription?.id
-  const kor_translate = SkinTypeDescription?.kor_translate
-  const skin_barrier = SkinTypeDescription?.barrier
+  const skintype = SkinTypeDescription?.name;
+  const percentage = SkinTypeDescription?.per;
+  const skintype_id = SkinTypeDescription?.id;
+  const kor_translate = SkinTypeDescription?.kor_translate;
+  const skin_barrier = SkinTypeDescription?.barrier;
 
   return (
     <div className="mx-auto max-w-2xl px-4 mt-4">
-      <div className="flex flex-col gap-2 rounded-2xl bg-zinc-50 sm:p-8 p-4 text-sm sm:text-base">
-        <h1 className="text-2xl sm:text-2xl tracking-tight font-bold max-w-fit inline-block capitalize">
+      <div className="flex flex-col gap-2 rounded-2xl bg-zinc-50 sm:px-8 px-4 text-sm sm:text-base mt-4">
+        <h1 className="text-2xl sm:text-2xl tracking-tight font-bold max-w-fit inline-block capitalize mt-4">
           {skintype_id} : {skintype}
         </h1>
         <p>
-          한국의 <span className="text-xl font-semibold text-blue-500">{percentage}%</span>에 해당하는 피부타입이다. {' '}
-          {skintype} 피부타입은 {' '}
-          <span className="text-lg font-semibold text-blue-500">{kor_translate}</span>의 특성을 띄는 피부타입으로, 
-          주요한 피부 건강 취약점으로는 <span className="text-lg font-semibold text-red-500">{skin_barrier}</span>이 있다.
+          한국의{" "}
+          <span className="text-xl font-semibold text-blue-500">
+            {percentage}%
+          </span>
+          에 해당하는 피부타입입니다. {skintype} 피부타입은{" "}
+          <span className="text-lg font-semibold text-blue-500">
+            {kor_translate}
+          </span>
+          의 특성을 띄는 피부타입으로, 주요한 피부 건강 취약점은{" "}
+          <span className="text-lg font-semibold text-red-500">
+            {skin_barrier}
+          </span>
+          입니다.
         </p>
         <p className="leading-normal text-zinc-900">
           더 자세한 사항은{" "}
@@ -56,12 +65,8 @@ export function SkinTypeHeader({ matchingType }: { matchingType: any }) {
         <p>{SkinTypeDescription?.pros}</p>
         <p>{SkinTypeDescription?.cons}</p>
         */}
-        <h1 className="text-2xl sm:text-2xl tracking-tight font-bold max-w-fit inline-block capitalize mt-4">
-          Enhancing Ingredients : {skintype}
-        </h1>
-        <p>{SkinTypeDescription?.recommendedIngredients}</p> 
       </div>
-      <div className="flex flex-col gap-2 rounded-2xl bg-zinc-50 sm:p-8 p-4 text-sm sm:text-base mt-4">
+      <div className="flex flex-col gap-2 rounded-2xl bg-zinc-50 sm:px-8 px-4 text-sm sm:text-base mt-4">
         <div className="morning-routine">
           <h1 className="text-2xl sm:text-2xl tracking-tight font-bold max-w-fit inline-block capitalize mt-4">
             {matchingType.slug} 추천 모닝루틴
@@ -100,8 +105,26 @@ export function SkinTypeHeader({ matchingType }: { matchingType: any }) {
               );
             })}
           </div>
+          <div>
+            <img className="size-1/2" src="/img/footer.png" alt="footer" />
+          </div>
         </div>
       </div>
+      {/* <div className="flex flex-col gap-2 rounded-2xl bg-zinc-50 sm:px-8 px-4 text-sm sm:text-base mt-4">
+        <div className="morning-routine">
+          <h1 className="text-2xl sm:text-2xl tracking-tight font-bold max-w-fit inline-block capitalize mt-4">
+            Enhancing Ingredients : {skintype}
+          </h1>
+          <p>
+            {SkinTypeDescription?.recommendedIngredients}
+          </p>
+          <p>성분1</p>
+          <p>성분2</p>
+          <p>성분3</p>
+          <p>성분4</p>
+          <p>성분5</p>
+        </div>
+      </div> */}
     </div>
   );
 }
