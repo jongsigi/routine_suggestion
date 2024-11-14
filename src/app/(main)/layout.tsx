1dcffc70f4761b3c3a1e8c8b89d2b97cdbf3cd29
jongsigi <jongsigi500@gmail.com>
2024-11-14 23:35:07 +0900

    241114 fix meta tag opengraph

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -4,10 +4,30 @@ import { Header } from "@/ui/components/Header";
 export const metadata = {
 	title: "SiGi SkinCare",
 	description: "K-Beauty Skincare Solution. Define your skintype and use right skincare routine.",
-    other: {
-		"google-adsense-account": "ca-pub-1977720073946887", // Replace 'aaa' with your actual account ID
+	openGraph: {
+	  type: "website",
+	  url: "https://sigiskincare.store",
+	  title: "SiGi SkinCare",
+	  description: "K-Beauty Skincare Solution. Define your skintype and use right skincare routine.",
+	  images: [
+		{
+		  url: "/img/share.webp", // Place this image in the public folder
+		  width: 1200,
+		  height: 630,
+		  alt: "SiGi Skincare Brand image",
+		},
+	  ],
 	},
-};
+	twitter: {
+	  card: "summary_large_image",
+	  title: "SiGi SkinCare",
+	  description: "K-Beauty Skincare Solution. Define your skintype and use right skincare routine.",
+	  images: ["/img/share.webp"], // This should match your Open Graph image
+	},
+	other: {
+	  "google-adsense-account": "ca-pub-1977720073946887", // Replace 'aaa' with your actual account ID
+	},
+  };
 
 export default function RootLayout(props: { children: ReactNode }) {
     return (
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,14 +2,6 @@
 import SkinTypesCollection from "@public/SkinTypesCollection.json";
 import { SkinTypeList } from "@/ui/components/SkinTypeList";
 
-export const metadata = {
-  title: "SiGi SkinCare",
-  description: "SiGi SkinCare",
-  other: {
-		"google-adsense-account": "ca-pub-1977720073946887", // Replace 'aaa' with your actual account ID
-	},
-};
-
 export default function Page({ params }: { params: { channel: string } }) {
   if (!SkinTypesCollection.collection?.types) {
     return null;
