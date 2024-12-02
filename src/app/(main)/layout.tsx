89293728454793d0899a4e0c351b3bef0372946e
jongsigi <jongsigi500@gmail.com>
2024-12-02 22:44:44 +0900

    241202 Change meta data position

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,34 +1,6 @@
 import { type ReactNode } from "react";
 import { Header } from "@/ui/components/Header";
 
-export const metadata = {
-	title: "SiGi SkinCare",
-	description: "피부타입 검사부터 스킨케어 제품 추천까지",
-	openGraph: {
-	  type: "website",
-	  url: "https://sigiskincare.store",
-	  title: "SiGi SkinCare",
-	  description: "피부타입 검사부터 스킨케어 제품 추천까지",
-	  images: [
-		{
-		  url: "/img/share.webp", // Place this image in the public folder
-		  width: 1200,
-		  height: 630,
-		  alt: "SiGi Skincare Brand image",
-		},
-	  ],
-	},
-	twitter: {
-	  card: "summary_large_image",
-	  title: "SiGi SkinCare",
-	  description: "피부타입 검사부터 스킨케어 제품 추천까지",
-	  images: ["/img/share.webp"], // This should match your Open Graph image
-	},
-	other: {
-	  "google-adsense-account": "ca-pub-1977720073946887", // Replace 'aaa' with your actual account ID
-	},
-  };
-
 export default function RootLayout(props: { children: ReactNode }) {
     return (
         <>
