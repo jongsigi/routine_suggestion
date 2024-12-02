cd9f6a7cc1c850528a4f7be1b676e0a0e39bf5e7
jongsigi <jongsigi500@gmail.com>
2024-12-02 23:28:37 +0900

    241202 Add Google analytics at top layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata = {
     description: "피부타입 검사부터 스킨케어 제품 추천까지",
     images: [
       {
-        url: "/img/share.webp", // Place this image in the public folder
+        url: "/img/share.webp",
         width: 1200,
         height: 630,
         alt: "SiGi Skincare Brand image",
@@ -26,23 +26,42 @@ export const metadata = {
     card: "summary_large_image",
     title: "SiGi SkinCare",
     description: "피부타입 검사부터 스킨케어 제품 추천까지",
-    images: ["/img/share.webp"], // This should match your Open Graph image
-  },
-  other: {
-    "google-adsense-account": "ca-pub-1977720073946887", // Replace 'aaa' with your actual account ID
+    images: ["/img/share.webp"],
   },
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className="min-h-dvh">
-      <body className={`${inter.className} min-h-dvh`}>
-        {children}
+      <head>
+        {/* Google Tag Manager */}
         <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1977720073946887"
-          crossOrigin="anonymous"
+          id="gtm-script"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NGXP6VXK');
+            `,
+          }}
         />
+        {/* End Google Tag Manager */}
+      </head>
+      <body className={`${inter.className} min-h-dvh`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NGXP6VXK"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        {children}
       </body>
     </html>
   );
