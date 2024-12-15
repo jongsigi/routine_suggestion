a8fbed47909d9ec2652d32a3ee7fc02c44b4462f
jongsigi <jongsigi500@gmail.com>
2024-12-15 17:32:53 +0900

    241215 Fix3 Google Adsense (Relocate Head tag)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,15 +32,6 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className="min-h-dvh">
-      <head>
-        {/* Google AdSense */}
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1977720073946887"
-          crossOrigin="anonymous" // Note the corrected camelCase: crossOrigin
-        />
-        {/* End Google AdSense */}
-      </head>
       <body className={`${inter.className} min-h-dvh`}>
         {children}
       </body>
