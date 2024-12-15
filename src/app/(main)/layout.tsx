72f75741ecc37ff100b0377e46058f89557555e3
jongsigi <jongsigi500@gmail.com>
2024-12-15 18:22:14 +0900

    241215 Top layout head tag

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -4,15 +4,6 @@ import { Header } from "@/ui/components/Header";
 export default function RootLayout(props: { children: ReactNode }) {
   return (
     <>
-      <head>
-        {/* Google AdSense */}
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1977720073946887"
-          crossOrigin="anonymous" // Note the corrected camelCase: crossOrigin
-        />
-        {/* End Google AdSense */}
-      </head>
       <Header />
       <div className="flex min-h-[calc(100dvh-64px)] flex-col">
         <main className="flex-1">{props.children}</main>
